5d721d2de9751549e5b7187082ee1d6bd93977cd
AlbertMCSmets <albert.smets@unifiedpost.com>
2018-07-24 12:06:01 +0200

    introduce application module

--- src/ApplicationModule.kt
+++ src/ApplicationModule.kt
@@ -1,23 +1,25 @@
 package com.up
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.auth.UserPasswordCredential
 import io.ktor.auth.authenticate
 import io.ktor.auth.authentication
 import io.ktor.auth.jwt.jwt
-import io.ktor.jackson.*
-import io.ktor.features.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.jackson.jackson
 import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.route
+import io.ktor.routing.routing
 import org.kodein.di.direct
 import org.kodein.di.generic.instance
-
-fun main(args: Array<String>): Unit = io.ktor.server.netty.DevelopmentEngine.main(args)
-
-val ApplicationCall.user get() = authentication.principal<User>()!!
 
 fun Application.module() {
     val kodeinAware = KodeinInjector().getKodeinAware(this.environment.config)
@@ -25,11 +27,11 @@ fun Application.module() {
     val jwtConfigStore = kodeinAware.direct.instance<JwtConfigStore>()
     val jwtAssist = kodeinAware.direct.instance<JwtAssist>()
 
-	install(ContentNegotiation) {
-		jackson {
-			enable(SerializationFeature.INDENT_OUTPUT)
-		}
-	}
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
 
     authentication {
         jwt("jwt") {
@@ -41,29 +43,26 @@ fun Application.module() {
         }
     }
 
-	routing {
-		get("hello") {
-			call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-		}
+    routing {
+        get("hello") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
 
-		get("helloJSON") {
-			call.respond(mapOf("hello" to "world"))
-		}
+        get("helloJSON") {
+            call.respond(mapOf("hello" to "world"))
+        }
 
         post("login") {
             call.respondText(userStore.authenticateUser(call.receive<UserPasswordCredential>())?.let(jwtAssist::createToken)?:"")
         }
 
-		authenticate("jwt") {
-			route("authorized"){
+        authenticate("jwt") {
+            route("authorized"){
                 get {
-                    val user = call.user
+                    val user = call.authentication.principal<User>()!!
                     call.respondText("${user.name} Authorized",contentType = ContentType.Text.Plain)
                 }
             }
-		}
-	}
+        }
+    }
 }
-
-
-
---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+package com.up
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.DevelopmentEngine.main(args)
+
+
+
+
+
