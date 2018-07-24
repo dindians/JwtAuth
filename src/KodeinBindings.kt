88f306837e3a5b344ca5990502138c22f47846dd
dindians <37927921+dindians@users.noreply.github.com>
2018-07-24 22:26:01 +0200

    Update KodeinBindings.kt
    removed unused code

--- src/KodeinBindings.kt
+++ src/KodeinBindings.kt
@@ -8,11 +8,7 @@ class KodeinBindings() {
     fun getKodein(applicationConfig: ApplicationConfig) = Kodein {
         constant(tag = "jwtSigningAlgorithmSecret") with "zAP5MBA4B4Ijz0MZaS48"
         bind<UserProvider>() with provider { UserProviderImpl() }
-        bind<JwtPropsProvider>() with provider {
-            val x = receiver
-            val y = context
-            JwtPropsProviderImpl(applicationConfig)
-        }
+        bind<JwtPropsProvider>() with provider { JwtPropsProviderImpl(applicationConfig) }
         bind<JwtSigningAlgorithmProvider>() with provider { JwtSigningAlgorithmProviderImpl(this.instance(tag = "jwtSigningAlgorithmSecret")) }
         bind<JwtIssuer>() with provider { JwtIssuerImpl(this.instance<JwtPropsProvider>(), this.instance<JwtSigningAlgorithmProvider>()) }
     }
