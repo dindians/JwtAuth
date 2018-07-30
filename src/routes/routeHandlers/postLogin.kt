4d4504f7666f83392e457ed8254912bde9a3a428
dindians <37927921+dindians@users.noreply.github.com>
2018-07-30 14:09:17 +0200

    changes login response to JSON format

--- src/routes/routeHandlers/postLogin.kt
+++ src/routes/routeHandlers/postLogin.kt
@@ -7,9 +7,9 @@ import io.ktor.application.call
 import io.ktor.auth.UserPasswordCredential
 import io.ktor.locations.post
 import io.ktor.request.receive
-import io.ktor.response.respondText
+import io.ktor.response.respond
 import io.ktor.routing.Route
 
 fun Route.postLogin(userAuthenticator: UserAuthenticator, jwtIssuer: JwtIssuer) = post<Login> { _ ->
-    call.respondText(userAuthenticator.authenticateUser(call.receive<UserPasswordCredential>())?.let(jwtIssuer::createToken)?:"")
+    call.respond(mapOf("bearerToken" to (userAuthenticator.authenticateUser(call.receive<UserPasswordCredential>())?.let(jwtIssuer::createToken)?:"")))
 }
