6c20924d15bce4bc93e7d5a9d32b39a1e9d307f0
dindians <37927921+dindians@users.noreply.github.com>
2018-08-08 17:01:01 +0200

    added optional parameters to /hello location

--- src/routes/locationData/Hello.kt
+++ src/routes/locationData/Hello.kt
@@ -3,4 +3,4 @@ package com.up.routes.locationData
 import io.ktor.locations.Location
 
 @Location("/hello/{name}")
-data class Hello(val name:String)
+data class Hello(val name:String, val age: Int=13, val color: String="blue")
